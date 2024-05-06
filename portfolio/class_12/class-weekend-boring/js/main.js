f3d61b983beafcf82ef435785424d24314f76285
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-05-06 18:03:56 +0500

    day mode check

--- portfolio/class_12/class-weekend-boring/js/main.js
+++ portfolio/class_12/class-weekend-boring/js/main.js
@@ -1,10 +1,19 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector("#check").addEventListener("click", check);
 
 function check() {
-
-  const day = document.querySelector('#day').value
+  const day = document.querySelector("#day").value;
 
   //Conditionals go here
+  if (day == "tuesday" || day == "thursday") {
+    console.log("ClassDay👍");
+    document.querySelector("#placeToSee").innerText = "ClassDay👍";
+  } else if (day == "saturday" || day == "sunday") {
+    console.log("Weekend😊");
 
-
+    document.querySelector("#placeToSee").innerText = "Weekend😊";
+    document.querySelector("#placeToSee").style.color = 'green';
+  } else {
+    console.log("Boooooooooooooooring😡");
+    document.querySelector("#placeToSee").innerText = "Boooooooooooooooring😡";
+  }
 }
