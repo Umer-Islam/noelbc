cdd343e324f5f03f8381877aa6146e791d6d396a
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-05-12 06:38:02 +0500

    basic temp convertor

--- portfolio/class_13/temperature-converter/js/main.js
+++ portfolio/class_13/temperature-converter/js/main.js
@@ -1,1 +1,14 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+// 1. input value
+document.querySelector('button').addEventListener('click',convert)
+
+function convert (){
+    initial_temp = document.querySelector('#tempC').value;
+    final_temp = (initial_temp * 9/5) +32
+    document.querySelector('h2').innerText  = final_temp
+
+
+
+}
+
+// 2. convert and display
