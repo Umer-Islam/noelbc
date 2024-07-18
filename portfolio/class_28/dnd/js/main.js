8fe4b0d49a6b9ea2342d37c528b6bb5cd2fa68e1
umer islam <139012668+Umer-Islam@users.noreply.github.com>
2024-07-18 20:05:04 +0500

    class28 practice

--- portfolio/class_28/dnd/js/main.js
+++ portfolio/class_28/dnd/js/main.js
@@ -1,17 +1,22 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      console.log(data.subclasses);
 
+      document.querySelector("p").innerText = data.desc[0]
+      document.querySelector("h2").innerText = data.name
+      document.querySelector("h4").innerText = data.subclasses[0].name;
+      document.querySelector("h3").innerText = data.classes[0].name;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
