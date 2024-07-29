5bc711eb5e3ce56c492b9d0b1dca6112b83357ef
umer islam <139012668+Umer-Islam@users.noreply.github.com>
2024-07-29 10:38:17 +0500

    class 29 practice

--- portfolio/class_29/class29-materials/thunder-badge/js/main.js
+++ portfolio/class_29/class29-materials/thunder-badge/js/main.js
@@ -1,2 +1,16 @@
 //Objects
 //Please create a pokemon class with as much detail as possible in 10 minutes
+class Pokemon{
+    constructor(name,color,power){
+        this.name = name;
+        this.color = color;
+        this.power = power;
+    }
+    intro = _=>{
+        console.log(`color of ${this.name} is ${this.color} and its power is ${this.power}`)
+    }
+}
+
+let firstChar = new Pokemon("picachu","yellow","⚡ ")
+console.log(firstChar)
+console.log(firstChar.intro())
