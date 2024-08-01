f0a83d33d437dc984e7e99a38112345c41a68283
umer islam <139012668+Umer-Islam@users.noreply.github.com>
2024-08-02 04:40:42 +0500

    31 practice

--- portfolio/class_31/objects-child-class/js/main.js
+++ portfolio/class_31/objects-child-class/js/main.js
@@ -1,2 +1,29 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 
+class FarmAnimals {
+  constructor(name,sound) {
+    this.name = name;
+    this.sound = sound;
+
+  }
+
+  intro=_=>{
+    console.log(`${this.name} names this sound ${this.sound}`)
+
+
+
+  }
+}
+
+
+class Birds extends FarmAnimals{
+constructor(canFly){
+    super(name);
+    super(sound);
+    this.canFly = canFly;
+}
+
+
+}
+
+let cat1 = new FarmAnimals('catte','mewo')
